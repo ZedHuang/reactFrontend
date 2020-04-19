fb9ff3c5b4a90ed73f6091948eba1405114548ef
ZedHuang <42935901+ZedHuang@users.noreply.github.com>
2020-04-18 22:30:05 -0400

    3

--- src/components/Billboard.js
+++ src/components/Billboard.js
@@ -9,15 +9,15 @@ export default function Billboard(props){
 
   return(
     <>
-    <Card className="text-center">
+    <Card className="text-center"  bg="light" text="dark">
         <Image src="https://upload.wikimedia.org/wikipedia/commons/2/2b/Billboard_Hot_100_logo.jpg"></Image>
         {props.songs.map((song,index)=>(
         <Row key={index+400} style={{marginLeft:'30px'}}>
             <Card.Img key={index+500} variant="bottom" src={musicIcon} style={{height:'7rem',width:'7rem',marginTop:'2px'}} key={index}></Card.Img>
             <Card.Body key={index+300}>
-                <Card.Title key={index}>{song[0]}</Card.Title>
-                <Card.Title key={index+100}>{song[1]}</Card.Title>
-                <Card.Text key={index+200}>{song[2]}</Card.Text>
+                <Card.Title  key={index}>{song[0]}</Card.Title>
+                <Card.Title  key={index+100}>{song[1]}</Card.Title>
+                <Card.Text   key={index+200}>{song[2]}</Card.Text>
                 <Button variant="primary" onClick={()=>props.handleRating(song[1],song[2])}>Get your rating</Button>
             </Card.Body>
         </Row>
